5816904e97dcb0997c9a466fda7f56d44e865ff0
Nazanin <n.beigirad@gmail.com>
2021-08-29 21:49:31 +0430

    try lessons 1-3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,59 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
 
 @ExperimentalUnitApi
-fun main() = Window {
+fun main() = Window(title = "Hello Compose Application") {
     MaterialTheme {
-        Text("This is JetPack Compose!")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Header()
+            Text("This is JetPack Compose!")
+            Image(
+                bitmap = imageResource("flower.jpg"),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(all = 8.dp)
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+
+            Row {
+                Button(onClick = {}) {
+                    Text("Run")
+                }
+                Spacer(modifier = Modifier.size(width = 8.dp, 1.dp))
+                Button(onClick = {}) {
+                    Text("End")
+                }
+            }
+        }
     }
+}
+
+@ExperimentalUnitApi
+@Composable
+private fun Header() {
+    Text(
+        "Hello World!",
+        fontSize = TextUnit(25f, TextUnitType.Sp),
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.width(IntrinsicSize.Max)
+    )
 }
